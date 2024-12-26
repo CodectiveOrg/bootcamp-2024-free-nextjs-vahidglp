beef46f0c67d753f25bc4bf6a0f88728f3068828
vahid golpayegani <vahidglp@gmail.com>
2024-12-26 11:54:43 +0330

    Lesson 2 (#4)
    * [CI/CD] [ADD]  lint.yml
    
    * [ADD] postcss nesting
    
    * [ADD] reset global.css
    
    * [ADD] dark mode
    
    * [REMOVE] unnecessary code and filss
    
    * [UPDATE] favicon
    
    * [ADD] header component
    
    * [ADD] footer component
    
    * [UPDATE] homepage content
    
    * [APP] prettier last update
    
    * [UPDATE] run prettier for all file
    
    * [UPDATE]
    
    * Update lint.yml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
+import { ReactElement } from "react";
+
 import { Vazirmatn } from "next/font/google";
-import "./globals.css";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
 import { NextFont } from "next/dist/compiled/@next/font";
+
+import "./globals.css";
 
 const vazirmatn: NextFont = Vazirmatn({
   subsets: ["latin", "arabic"],
@@ -17,10 +23,17 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>{children}</body>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
+      </body>
     </html>
   );
 }
